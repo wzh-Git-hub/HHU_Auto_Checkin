e618afb53b9ac1fd0effcf4f2a6ad5a72bb31d67
wzh-Git-hub <71327535+wzh-Git-hub@users.noreply.github.com>
2022-08-24 19:41:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def sign_in(uid, pwd):
             url=browser.current_url
             print(msg)
             print(url)
-	    time.sleep(1)
+            time.sleep(1)
             browser.find_element(by=NAME,value="name_RADIO_799044").click()
             time.sleep(1)
             browser.find_element_by_id("saveBtn").click()
